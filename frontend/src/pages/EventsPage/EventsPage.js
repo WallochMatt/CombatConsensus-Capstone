fc783f4498f2e821cc37823468a58f0467b15955
WallochMatt <matthewrwalloch@gmail.com>
2022-12-07 13:34:06 -0600

    feat: Finished Matches page fix: Fixed an issue where buttons were one large mass, a refactor to the html sections of those the effected pages were made to separate the buttons

--- frontend/src/pages/EventsPage/EventsPage.js
+++ frontend/src/pages/EventsPage/EventsPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 import "./EventsPage.css";
 import { useNavigate } from "react-router-dom";
 
@@ -7,7 +6,6 @@ const EventsPage = (props) => {
     let navigate = useNavigate();
 
     function waitLink(event){
-        console.log("clicked button");
         navigate(`/event-card/${event.id}/`);
     }
 
@@ -17,15 +15,17 @@ const EventsPage = (props) => {
 
             <div className="centerize">
                 <h1 className="true-center">EVENTS PAGE</h1>
-                <div className="format-ep">
-                {props.events &&
-                props.events.map((event, index) => (
-                    <button className="button" onClick={() => waitLink(event)} key={index}>
-                        <p >
-                            {event.event_title}
-                        </p>
-                    </button>
-                ))}
+                <div className="format">
+                    {props.events &&
+                    props.events.map((event, index) => (
+                        <div className="blurbs" key={index}>
+                            <button className="button" onClick={() => waitLink(event)} >
+                                <p >
+                                    {event.event_title}
+                                </p>
+                            </button>
+                        </div>
+                    ))}
                 </div>
             </div>
 
--- frontend/src/pages/MatchesPage/MatchesPage.js
+++ frontend/src/pages/MatchesPage/MatchesPage.js
@@ -1,20 +1,35 @@
 import React from "react";
+import "./MatchesPage.css"
+import { useNavigate } from "react-router-dom";
 
+const MatchesPage = (props) => {    
+    let navigate = useNavigate();
 
-const MatchesPage = (props) => {
-
-    
+    function sendEvent(match){
+        navigate(`/event-card/${match.event_id}/`)
+    }
 // link matches to the event theyre from?
     return ( 
-        <div className="container">
-            <h1>Matches</h1>
-            {props.matches &&
-            props.matches.map((match, index) => (
-                <p key={index}>
-                    {match.bout_name}
-                </p>
+        <div className="flex matches-pics">
+            <div className="side"></div>
+
+            <div className="centerize">
+                <h1 className="true-center">Matches</h1>
                 
-            ))}
+                <div className="format">
+                    {props.matches &&
+                    props.matches.map((match, index) => (
+                        <div className="blurbs"  key={index}>
+                            <button className="button" onClick={() => sendEvent(match)}>
+                                <p>{match.bout_name}</p>
+                            </button>
+                        </div>
+                    ))}
+                </div>
+
+            </div>
+
+            <div className="side"></div>
         </div>
     );
 }
